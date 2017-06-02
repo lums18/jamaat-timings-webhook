8cffd8dd9ecfb98edb18f2bd2891fa98b98a2be8
Abu Bakar <lums18@gmail.com>
2017-06-02 11:19:00 +0500

    the working flask app
    copied this from soonwhang and changed a few lines to make it run the
    current version of python-telegram-bot

---
+++ webhooks-echo-1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3.6
+
+import telegram
+from flask import Flask, request
+
+chinkubot='162676774:AAH7oiRemjTuV2vbfwqaABk46v_ceiLr_3c'
+TOKEN=chinkubot
+HOOK=chinkubot
+URL='abnaeem0.pythonanywhere.com/'
+
+global bot
+bot = telegram.Bot(token=TOKEN)
+
+def is_new_user(chat_id):
+    return True
+
+app = Flask(__name__)
+
+@app.route('/'+HOOK, methods=['POST'])
+def webhook_handler():
+    if request.method == "POST":
+        # retrieve the message in JSON and then transform it to Telegram object
+        update = telegram.Update.de_json(request.get_json(force=True),bot)
+
+        chat_id = update.message.chat.id
+        if is_new_user(chat_id):
+            bot.sendMessage(chat_id=chat_id,text='Searching for Masjids')
+
+        # Telegram understands UTF-8, so encode text for unicode compatibility
+        text = update.message.text  ##.encode('utf-8')
+
+        # repeat the same message back (echo)
+        bot.sendMessage(chat_id=chat_id, text=text)
+
+    return 'ok'
+    ##https://github.com/sooyhwang/Simple-Echo-Telegram-Bot
+
+
+@app.route('/set_webhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('https://'+URL+'/'+HOOK)
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
+
+
+@app.route('/')
+def index():
+    return '.'
